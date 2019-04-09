565154cbb0c18f97c35527792a9f01f3b65658af
Ramon <ramonrune@gmail.com>
2019-04-08 21:42:05 -0300

    formulario

---
+++ formulario/src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+
+class Cliente {
+  nome: string;
+  email: string;
+  profissao: string;
+}
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  cliente = new Cliente();
+
+  profissoes = ['Programador', 'Empresário', 'Outra'];
+
+  constructor() {
+    //this.cliente.profissao = 'Outra';
+  }
+
+  salvar(form: NgForm) {
+    //this.cliente.nome = form.value.nome;
+    //this.cliente.email = form.value.email;
+    //this.cliente.profissao = form.value.profissao;
+    //console.log(form);
+
+    console.log(form);
+  }
+}
